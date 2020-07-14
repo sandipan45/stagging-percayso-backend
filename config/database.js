422a139196019750591e6ea4c74e2ed59e2509b8
sandipan45 <sandipan45@gmail.com>
2020-07-14 17:52:01 +0530

    database updated

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     "default": {
       "connector": "bookshelf",
       settings: {
-        client: 'postgres',
+        client: 'mysql',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'strapi'),
